5f92bc072949647a363b32f9e8465f6c08375d93
Yury Reshetnikov <reshu@mail.ru>
2023-05-12 09:58:33 +0500

    _

--- src0/st/Primitive.hpp
+++ src0/st/Primitive.hpp
@@ -5,6 +5,9 @@ namespace st {
 	struct Form { // не путать с ru::Form - формой в общем виде
 	    // для представлений разных форм слова, в том числе склонений, времён и падежей
 	};
+	struct Meaning {
+	    // значения для многозначных слов
+	};
 	virtual const char *main_form() const { return "примитив"; }
     };
 }
