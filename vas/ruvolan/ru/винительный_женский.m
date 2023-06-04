1c1a405a687fd9a64efa75bd11807c9d7eeb21f5
Yury Reshetnikov (on build2) <yura.reshetnikov@portal59.ru>
2023-06-04 05:33:36 +0500

    _

---
+++ vas/ruvolan/ru/винительный_женский.m
@@ -0,0 +1,15 @@
+// Автор: Юрий Решетников <reshu@mail.ru>
+
+#parse_vas_macro
+    auto form = #Vasek::Reader::require(parser, Word)
+    parser.cmd_end()
+    #vas::todo
+    return Vasek::VasCodeEntityShp()
+
+/*
+ * Local Variables:
+ * mode: vasilek
+ * coding: utf-8-unix
+ * tab-width: 8
+ * End:
+ */
